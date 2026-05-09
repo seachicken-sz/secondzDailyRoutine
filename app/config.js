28dd556f2944cbbf51ac984c9da20154f5428b26
seachicken-sz <natsumi6315@gmail.com>
2026-05-09 21:01:07 +0900

    Create config.js

---
+++ app/config.js
@@ -0,0 +1,6 @@
+const SPOTIFY_TRACK_BASE_URL = "https://open.spotify.com/track/"; // SpotifyURL
+const USEN_REQUEST_BASE_URL = "https://usen.oshireq.com/song/"; //USENURL
+const X_POST_URL = "https://twitter.com/intent/tweet?text="; //Xポスリン用
+const THREADS_URL = "https://www.threads.net/"; //Threads開く用
+const YOUTUBE_THUMBNAIL_BASE_URL = "https://img.youtube.com/vi/"; //YouTubeサムネ取得用
+const TIMELESZ_SPOTIFY_ARTIST_URL = "https://open.spotify.com/intl-ja/artist/1ZFfhzyXjPvbzSYPlCIwo3"; //SpotifyアーティストURL
