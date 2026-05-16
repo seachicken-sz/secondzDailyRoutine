2dcebb11e97acbab2f0cf3be5e2a41ceeb6299a0
seachicken-sz <natsumi6315@gmail.com>
2026-05-17 00:14:38 +0900

    Update app.js

--- app/app.js
+++ app/app.js
@@ -740,7 +740,7 @@ function updateHomeInstallGuideVisibility() {
 async function init() {
   try {
     updateHomeInstallGuideVisibility();
-
+    initializeSetupGuide();
     const songs = await loadSpotifySongs();
     const recommendedSongs = songs.filter((song) => song.flag === true);
     const otherSongs = songs.filter((song) => song.flag !== true);
