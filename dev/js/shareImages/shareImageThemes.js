2a2f106d89715cc5b401a35865dfae8efc0764f0
seachicken-sz <natsumi6315@gmail.com>
2026-06-10 13:20:10 +0900

    Add files via upload

---
+++ dev/js/shareImages/shareImageThemes.js
@@ -0,0 +1,74 @@
+const SHARE_IMAGE_THEMES = {
+  normal: {
+    main: "#3fbfa8",
+    pale: "#f2fbf8",
+    dusty: "#78bfb0",
+    line: "#c9e7df",
+    dream: "#b9f0e4",
+    dreamSecond: "#d9f7ff"
+  },
+  red: {
+    main: "#e85b4f",
+    pale: "#fff7f5",
+    dusty: "#d79a9a",
+    line: "#ead0d0",
+    dream: "#ffc7c7",
+    dreamSecond: "#ffe1d6"
+  },
+  purple: {
+    main: "#8d72c7",
+    pale: "#faf7ff",
+    dusty: "#a99ab7",
+    line: "#ddd5e6",
+    dream: "#d9c7ff",
+    dreamSecond: "#ffd7ef"
+  },
+  green: {
+    main: "#5ca35a",
+    pale: "#f7fbf5",
+    dusty: "#91b483",
+    line: "#d5e6ce",
+    dream: "#c8efbd",
+    dreamSecond: "#dff7e6"
+  },
+  sky: {
+    main: "#5bb9e6",
+    pale: "#f4fbff",
+    dusty: "#91b9c9",
+    line: "#d4e7ee",
+    dream: "#bdeeff",
+    dreamSecond: "#e1dcff"
+  },
+  lime: {
+    main: "#9bcf45",
+    pale: "#fbfff3",
+    dusty: "#b0c77b",
+    line: "#e0ebc5",
+    dream: "#e0f7a8",
+    dreamSecond: "#fff0bd"
+  },
+  pink: {
+    main: "#ef8fa7",
+    pale: "#fff7fa",
+    dusty: "#d8a4b7",
+    line: "#ecd3dd",
+    dream: "#ffc9df",
+    dreamSecond: "#d9e2ff"
+  },
+  yellow: {
+    main: "#d9ad00",
+    pale: "#fffdf0",
+    dusty: "#d8c58f",
+    line: "#eee4c2",
+    dream: "#fff0aa",
+    dreamSecond: "#ffd8c7"
+  },
+white: {
+  main: "#b8b8b8",
+  pale: "#ffffff",
+  dusty: "#c4c4c0",
+  line: "#e1e1de",
+  dream: "#a5a5a5",
+  dreamSecond: "#b5b2b9"
+}
+};
