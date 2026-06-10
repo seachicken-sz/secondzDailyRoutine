1f9390f39593cc871c74b613be8a5ffbf0388516
seachicken-sz <natsumi6315@gmail.com>
2026-06-10 14:31:06 +0900

    Update index.js

--- dev/js/shareImages/index.js
+++ dev/js/shareImages/index.js
@@ -10,10 +10,20 @@
   const currentScript = document.currentScript;
   const basePath = currentScript.src.replace("index.js", "");
 
-  styleFiles.forEach((file) => {
+  function loadScript(index) {
+    if (index >= styleFiles.length) {
+      window.shareImageStylesLoaded = true;
+      window.dispatchEvent(new Event("shareImageStylesLoaded"));
+      return;
+    }
+
     const script = document.createElement("script");
-    script.src = `${basePath}${file}`;
-    script.defer = false;
+    script.src = `${basePath}${styleFiles[index]}`;
+    script.onload = () => loadScript(index + 1);
+    script.onerror = () => loadScript(index + 1);
     document.head.appendChild(script);
-  });
+  }
+
+  window.shareImageStylesLoaded = false;
+  loadScript(0);
 })();
