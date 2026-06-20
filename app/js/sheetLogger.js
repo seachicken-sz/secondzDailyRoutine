2d44ec1f96ca81964cd8e997b0dbb97c3e04c503
seachicken-sz <natsumi6315@gmail.com>
2026-06-21 05:43:33 +0900

    Update sheetLogger.js

--- app/js/sheetLogger.js
+++ app/js/sheetLogger.js
@@ -1,4 +1,4 @@
-const SHEET_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw4xCifrcANzTS5Ym7-Zta3j_qqfW2VOwVsZhFlAuLi9m1FOqV45aJB6BgsApmu_6qN/exec";
+const SHEET_WEB_APP_URL = "https://script.google.com/macros/s/AKfycby0SKCFzmUkXLA57D-ZrWPzyAr3ImOXPkiUA4Dx1h3goYPCuDiHYTr2Ui-LWmgcaARU/exec";
 const SHEET_TOKEN = "test-token";
 const SHEET_APP_NAME = "secondzDailyRoutine";
 const SHEET_CLIENT_ID_KEY = "secondzDailyRoutineClientId";
